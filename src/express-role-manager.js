1db4ea40290abff5758ce0ea45c79ffdf04e86a8
Carlos Martinez <carlos.spora@gmail.com>
2017-01-05 09:31:19 +0100

    Comments and refactor

--- src/express-role-manager.js
+++ src/express-role-manager.js
@@ -1,69 +1,80 @@
 module.exports = ExpressRoleManager;
 
-function ExpressRoleManager(options){
-    if(!this instanceof ExpressRoleManager) {
-        return new ExpressRoleManager(options);
-    }
+function ExpressRoleManager(conf){
+    // Ensure instance
+    if(!this instanceof ExpressRoleManager)
+        return new ExpressRoleManager(conf);
 
-    options = options || {};
+    // Init conf
+    this._conf = Object.assign({
+        callAllCommands: false,
+        defaultCommand: (req, res, next) => next()
+    }, conf);
 
-    var roleGetters = new Array();
-    var callAllCommands = Boolean(options.callAllCommands);
-    var defaultCommand = options.defaultCommand || function(req, res, next){
-        next();
-    };
+    // Init _roleGetters object
+    this._roleGetters = new Array();
 
     /**
-     * 
+     * Adds a role getter to the _roleGetters list.
      */
     this.addRoleGetter = function(){
+        // Handle function role getters
         if(arguments.length === 1) {
             if(typeof arguments[0] !== 'function')
                 throw new Error("Role getter must be a function.");
-            return roleGetters.push(arguments[0]);
+            return this._roleGetters.push(arguments[0]);
         }
+
+        // Handle key:functiion role getters
         if(typeof arguments[0] !== 'string' && typeof arguments[0] !== 'number')
             throw new Error("Role getter identifier must be a string or a number.");
         if(typeof arguments[1] !== 'function')
             throw new Error("Role getter must be a function.");
-        roleGetters.push({
+        this._roleGetters.push({
             role: arguments[0],
             function: arguments[1]
         });
     };
 
     /**
-     * 
+     * Returns a middleware that runs functions based on roles
      */
-    this.commandPerRole = function(commands){
-        return function(req, res, next) {
-            loadRequestRoles(req);
+    this.commandPerRole = function(commands, conf){
+        // Get exec config
+        var localConf = Object.assign({}, this._conf, conf);
+
+        // Generate express middleware
+        return (req, res, next) => {
+            // Load requester roles in request object
+            this._loadRequestRoles(req);
             
             // TODO run command
             console.log("Roles: ", req._expressRoleManager.roles);
 
-            defaultCommand(req, res, next);
+            localConf.defaultCommand(req, res, next);
         }
     }
 
     /**
-     * 
+     * Loads requester roles in request object
      */
-    function loadRequestRoles(req) {
+    this._loadRequestRoles = function(req) {
         req._expressRoleManager = {};
-        req._expressRoleManager.roles = obtainRequestRoles(req);
+        req._expressRoleManager.roles = this._obtainRequestRoles(req);
     }
 
     /**
-     * 
+     * Obtains roles from request
      */
-    function obtainRequestRoles(req) {
+    this._obtainRequestRoles = function(req) {
         var roles = [];
 
-        roleGetters.forEach(roleGetter => {
+        this._roleGetters.forEach(roleGetter => {
+            // Handle key:function role getters
             if(typeof roleGetter === 'object' && roleGetter.function(req))
                 return roles.push(roleGetter.role);
 
+            // Handle function role getters
             if(typeof roleGetter !== 'function') return;
             var rolesRes = roleGetter(req);
             if(typeof rolesRes === 'number' || typeof rolesRes === 'string')
